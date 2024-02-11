9237014bad81f0691298f65afa1e8fc371d9f4b3
LiiNi-coder <dlsgudrpdla@knu.ac.kr>
2024-02-12 02:09:22 +0900

    FEAT: 돌놓기구현완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function print2DArray(arr) {
+  for(let i = 0; i < arr.length; i++) {
+      console.log(arr[i].join(''));
+  }
+}
+
+function Square({value, onSquareClick}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-export default App;
+function Board() {
+  const initial_board = Array(15).fill(null).map(() => Array(15).fill("."));
+  
+  //states
+  const [squares, setSquares] = useState(initial_board);
+
+  //handler
+  function handleSquareClick(row, col, stone){
+    const new_squares = squares.map(r=>[...r]);
+    const new_row = new_squares[row].slice();
+    new_row[col] = stone;
+    new_squares[row] = new_row;
+    setSquares(new_squares);
+  }
+
+  useEffect(()=>{
+    print2DArray(squares);
+  });
+  //render
+  const oneRowRender = (index_row)=>{
+    const result = []
+    for (let index_col = 0; index_col<squares[index_row].length; index_col++){
+      result.push(<Square value={squares[index_row][index_col]} onSquareClick={()=>{handleSquareClick(index_row, index_col, "X");}}/>)
+    };
+    return result;
+  };
+  const RowsRender = ()=>{
+    const result = []
+    for(let index_row = 0; index_row<squares.length; index_row++){
+      result.push(<div className='board-row'>{oneRowRender(index_row)}</div>);
+    };
+    return result
+  };
+
+  //return
+  return (
+    <>
+      {RowsRender()}
+    </>
+  );
+}
+
+export default Board;
