9b623349ac34b9acf23f7cd576a9ea9d2bb6b2ac
LiiNi-coder <dlsgudrpdla@knu.ac.kr>
2024-02-12 15:56:52 +0900

    Logic: 컴포넌트id추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function print2DArray(arr) {
-  for(let i = 0; i < arr.length; i++) {
-      console.log(arr[i].join(''));
-  }
-}
-
 function Square({value, onSquareClick}){
   return (
     <button className="square" onClick={onSquareClick}>
@@ -17,37 +11,43 @@ function Square({value, onSquareClick}){
 }
 
 function Board() {
-  const initial_board = Array(15).fill(null).map(() => Array(15).fill("."));
-  
+  const initial_board = Array(15).fill(null).map(() => Array(15).fill(null));
+  var initial_stone = false //false:white, true:black
   //states
   const [squares, setSquares] = useState(initial_board);
+  const [isNowBlack, setIsNowBlack] = useState(initial_stone);
 
   //handler
-  function handleSquareClick(row, col, stone){
+  function handleSquareClick(row, col){
+    //놓은곳을 또 놓으려할때 처리
+    if(squares[row][col] !== null)
+      return;
+
+    //불변성위한 복제 및 돌 놓기
     const new_squares = squares.map(r=>[...r]);
     const new_row = new_squares[row].slice();
-    new_row[col] = stone;
+    new_row[col] = isNowBlack? "⚫" : "⚪";
     new_squares[row] = new_row;
+
+    //상태변환
     setSquares(new_squares);
+    setIsNowBlack(!isNowBlack);
   }
 
-  useEffect(()=>{
-    print2DArray(squares);
-  });
   //render
   const oneRowRender = (index_row)=>{
     const result = []
     for (let index_col = 0; index_col<squares[index_row].length; index_col++){
-      result.push(<Square value={squares[index_row][index_col]} onSquareClick={()=>{handleSquareClick(index_row, index_col, "X");}}/>)
+      result.push(<Square value={squares[index_row][index_col]} key={`${index_row} ${index_col}`} onSquareClick={()=>{handleSquareClick(index_row, index_col);}}/>)
     };
     return result;
   };
   const RowsRender = ()=>{
     const result = []
     for(let index_row = 0; index_row<squares.length; index_row++){
-      result.push(<div className='board-row'>{oneRowRender(index_row)}</div>);
+      result.push(<div key={`${index_row}`}className='board-row'>{oneRowRender(index_row)}</div>);
     };
-    return result
+    return result;
   };
 
   //return
